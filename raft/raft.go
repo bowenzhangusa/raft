09a3f07b7002675959b52d332d538caca86ce4c6
Anton Sergeyev <sergeyev.anton.s@gmail.com>
2018-03-02 16:05:21 -0800

    initial RequestVote implementation

--- raft/raft.go
+++ raft/raft.go
@@ -18,7 +18,11 @@ package raft
 //
 
 import "sync"
-import "../labrpc"
+import (
+	"../labrpc"
+	"time"
+	"math/rand"
+)
 
 //
 // as each Raft peer becomes aware that successive log entries are
@@ -37,8 +41,8 @@ type ApplyMsg struct {
 // and  a term when the entry is received by the leader
 //
 type Log struct {
-	Command     interface{}
-	term        int // term when the entry is received by the leader, starts at 1
+	Command interface{}
+	term    int // term when the entry is received by the leader, starts at 1
 }
 
 //
@@ -49,22 +53,23 @@ type Raft struct {
 	peers []*labrpc.ClientEnd // RPC end points of all peers
 	me    int                 // this peer's index into peers[]
 
-	currentTerm   int //This is the term number starting at 1
-	votedFor      int //CandidateId that this server voted for in this term
-	logEntries    []Log
+	currentTerm int //This is the term number starting at 1
+	votedFor    int //CandidateId that this server voted for in this term
+	logEntries  []Log
 
 	// The following variables are volatile states on all servers
 	// Both of the following indices increase monotonically and cannot decrease or go back
-	commitIndex   int // index of highest log entry known to be committed
-	lastApplied   int // index of the highest log entry applied to state machines
+	commitIndex int // index of highest log entry known to be committed
+	lastApplied int // index of the highest log entry applied to state machines
 
 	// The following are leader related properties
-	isLeader      bool
-	nextIndex     []int // for each server, index of the next log entry to send to that server
-						// initialzed to leader's lasst log index + 1
-	matchIndex    []int // for each server, index of highest log entry known to be replicated on that server
-						// initialized to zero, increases monotonically
+	isLeader  bool
+	nextIndex []int // for each server, index of the next log entry to send to that server
+	// initialzed to leader's lasst log index + 1
+	matchIndex []int // for each server, index of highest log entry known to be replicated on that server
+	// initialized to zero, increases monotonically
 
+	appendEntriesChannel chan *AppendEntriesArgs
 }
 
 // return currentTerm and whether this server
@@ -79,26 +84,26 @@ func (rf *Raft) GetState() (int, bool) {
 
 // example AppendEntriesRPC arguments structure
 type AppendEntriesArgs struct {
-	Term              int // term number
-	LeaderId          int // id of the leader
-	PrevLogIndex      int // index of the log immediately preceding new ones
-	PrevLogTerm       int // term of prevLogIndex entry
+	Term              int   // term number
+	LeaderId          int   // id of the leader
+	PrevLogIndex      int   // index of the log immediately preceding new ones
+	PrevLogTerm       int   // term of prevLogIndex entry
 	LogEntries        []Log //log entries to store. For heartbeat, this is empty. May send more than one for efficiency
 	LeaderCommitIndex int
-
 }
 
 // example AppendEntriesRPC reply structure
 type AppendEntriesReply struct {
-	Term      int // term number
-	Success   bool //true if follower contains log entry matching PrevLogIndex and PrevLogTerm
+	Term    int  // term number
+	Success bool //true if follower contains log entry matching PrevLogIndex and PrevLogTerm
 }
 
 //
 // example RequestVote RPC handler.
 //
 func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
-	// Your code here (3A, 3B).
+	rf.appendEntriesChannel <- args
+
 }
 
 // example RequestVote RPC arguments structure.
@@ -116,8 +121,8 @@ type RequestVoteArgs struct {
 // field names must start with capital letters!
 //
 type RequestVoteReply struct {
-	Term         int // term number of the election
-	VoteGranted  bool // If the vote is granted
+	Term        int  // term number of the election
+	VoteGranted bool // If the vote is granted
 }
 
 //
@@ -125,6 +130,7 @@ type RequestVoteReply struct {
 //
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 	// Your code here (3A, 3B).
+	DPrintf("Request vote received from %d", args.CandidateId)
 }
 
 //
@@ -194,6 +200,50 @@ func (rf *Raft) Kill() {
 	// Your code here, if desired.
 }
 
+func (rf *Raft) listen() {
+	for {
+		select {
+		case args := <-rf.appendEntriesChannel:
+			DPrintf("AppendEntries received: %+v", args)
+
+		case <-time.After(time.Duration(700+rand.Intn(300)) * time.Millisecond):
+			DPrintf("Timeout, will request votes")
+			rf.becomeCandidate()
+		}
+	}
+}
+
+func (rf *Raft) becomeCandidate() {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	rf.currentTerm++
+	rf.votedFor = rf.me
+	var lastLogTerm int
+
+	if len(rf.logEntries) > 0 {
+		lastLogTerm = rf.logEntries[len(rf.logEntries)-1].term
+	} else {
+		lastLogTerm = 0
+	}
+
+	args := RequestVoteArgs{
+		CandidateId:  rf.me,
+		LastLogTerm:  lastLogTerm,
+		LastLogIndex: len(rf.logEntries) - 1,
+	}
+	responses := make([]RequestVoteReply, len(rf.peers))
+
+	for i, _ := range rf.peers {
+		if i == rf.me {
+			continue
+		}
+
+		DPrintf("Sending vote request to %d", i)
+		rf.sendRequestVote(i, &args, &responses[i])
+	}
+}
+
 //
 // the service or tester wants to create a Raft server. the ports
 // of all the Raft servers (including this one) are in peers[]. this
@@ -207,7 +257,9 @@ func Make(peers []*labrpc.ClientEnd, me int, applyCh chan ApplyMsg) *Raft {
 	rf := &Raft{}
 	rf.peers = peers
 	rf.me = me
+	rf.appendEntriesChannel = make(chan *AppendEntriesArgs, 1)
 
+	go rf.listen()
 	// Your initialization code here (3A, 3B).
 
 	return rf
